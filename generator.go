80b1479bf73576df55b46160cbafec2e0af450da
atmatto <mattoover@gmail.com>
2021-12-15 18:26:33 +0100

    Zmiany w generatorze - w pełni działające menu nawigacyjne

--- generator.go
+++ generator.go
@@ -17,10 +17,10 @@ import (
 var format string
 
 type navElement struct {
-	name     string       // file/directory name, relative to its parent; directories contain trailing slash
-	link     string       // empty - no link; may be equal to name or dir/index.html for directories
+	name     string       // file/directory name, directories contain trailing slash
+	link     bool         // true if the directory has an index.html child, true for all files
 	title    string       // pretty title
-	children []navElement // does not contain index.html for directories
+	children []navElement // does not contain index.html
 }
 
 // root -> sekcja -> strony sekcji -> podstrony
@@ -57,24 +57,67 @@ func getConfigVars(text string) map[string]string {
 	return out
 }
 
-func navRec(currentPath string, current navElement, src string, relRoot string) string {
+func pathToSlice(path string) []string {
+	return strings.Split(path, string(os.PathSeparator))
+}
+
+func sliceToPath(path []string) string {
+	str := ""
+	for i, s := range path {
+		if i != 0 {
+			str += string(os.PathSeparator)
+		}
+		str += strings.Trim(s, string(os.PathSeparator))
+	}
+	return str
+}
+
+// Ta funkcja porównuje dwie ścieżki i zwraca:
+//    2 jeżeli są sobie równe
+//    1 jeżeli p2 jest rodzicem p1
+//    0 w innym przypadku
+// Traktuje ścieżkę do katalogu i ścieżkę do pliku
+// index.html w tym samym katalogu jako równe sobie ścieżki.
+func compareSlicePaths(p1 []string, p2 []string) int {
+	fmt.Println(p1, p2)
+	len1 := len(p1)
+	len2 := len(p2)
+	eq := 0
+	for i, s := range p2 {
+		if p1[i] == s {
+			eq++
+		} else {
+			break
+		}
+	}
+	if eq == len2 {
+		if len1 == len2 || ((len1-1 == len2) && p1[len1-1] == "index.html") {
+			return 2
+		}
+		return 1
+	}
+	return 0
+}
+
+// Ta funkcja służy do rekursywnego tworzenia menu nawigacyjnego na stronie o ścieżce `page`
+func navRec(path []string, current *navElement, page []string, relRoot string) string {
+	currentPath := append(path, strings.Trim(current.name, string(os.PathSeparator)))
 	str := "<li"
-	fmt.Println(current.link, currentPath+current.link, strings.SplitN(src, "/", 2)[1])
-	if currentPath+current.name == strings.SplitN(src, "/", 2)[1] ||
-		currentPath+current.name == "./"+strings.SplitN(src, "/", 2)[1] ||
-		(strings.Contains(current.link, "index.html") && currentPath+current.link == strings.SplitN(src, "/", 2)[1]) ||
-		(strings.Contains(current.link, "index.html") && currentPath+current.link == "./"+strings.SplitN(src, "/", 2)[1]) {
+	cmp := compareSlicePaths(page, currentPath)
+	if cmp == 2 {
 		str += " class=\"current\""
+	} else if cmp == 1 {
+		str += " class=\"parent\""
 	}
 	str += "><a"
-	if current.link != "" {
-		str += " href=\"" + relRoot + "/" + currentPath + current.link + "\""
+	if current.link {
+		str += " href=\"" + relRoot + string(os.PathSeparator) + sliceToPath(currentPath) + "\""
 	}
 	str += ">" + current.title + "</a></li>"
 	if len(current.children) != 0 {
 		str += "<li><ul>"
-		for _, c := range current.children {
-			str += navRec(filepath.Base(currentPath)+"/"+current.name, c, src, relRoot)
+		for i := range current.children {
+			str += navRec(currentPath, &current.children[i], page, relRoot)
 		}
 		str += "</ul></li>"
 	}
@@ -82,20 +125,24 @@ func navRec(currentPath string, current navElement, src string, relRoot string) 
 }
 
 // Ta funkcja tworzy menu nawigacyjne dla danej strony
-func navHTML(src string, relRoot string, section string) string {
-	for _, s := range nav.children {
+func navHTML(page string, relRoot string, section string) string {
+	for i, s := range nav.children {
 		if s.name == section+"/" {
-			return "<ul>" + navRec("", s, src, relRoot) + "</ul>"
+			return "<ul>" + navRec([]string{}, &nav.children[i], pathToSlice(page)[1:], relRoot) + "</ul>"
 		}
 	}
-	fmt.Println(src, section)
 	return ""
 }
 
+// Ta funkcja tworzy linki do różnych sekcji strony
 func navTop(relRoot string) string {
 	str := ""
 	for _, s := range nav.children {
-		str += "<a href=\"" + relRoot + "/" + s.link + "\">" + s.title + "</a>"
+		if s.link {
+			str += "<a href=\"" + relRoot + "/" + s.name + "\">" + s.title + "</a>"
+		} else {
+			fmt.Println("Warning: Section", s.title, "should have an index.html file.")
+		}
 	}
 	return str
 }
@@ -138,8 +185,6 @@ func generateFile(src string, dest string) error {
 	if filepath.Base(filepath.Dir(src)) != "src" {
 		section := strings.Split(src, "/")[1]
 		nav = "<nav>" + navHTML(src, relRoot, section) + "</nav>"
-	} else {
-		fmt.Println(filepath.Split(src))
 	}
 
 	replacer := strings.NewReplacer(
@@ -208,64 +253,59 @@ func getTitle(file string) string {
 	return conf["title"]
 }
 
-// reurns new children
-func directoryNavTree(dir string, parentsChildren []navElement) ([]navElement, error) {
+func directoryNavTree(dir string, e *navElement) error {
 	dirEntries, err := os.ReadDir(dir)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	dirName := filepath.Base(dir)
-
+	name := filepath.Base(dir)
 	if dir == "src" {
-		dirName = "."
+		name = ""
 	}
 
-	link := ""
-	title := dirName
-	if dirContains(dirEntries, "index.html") {
-		//link = "index.html"
-		link = dirName + "/index.html"
-		title = getTitle(dir + "/index.html")
-	}
-
-	thisDir := navElement{
-		name:     dirName + "/",
-		link:     link,
-		title:    title,
-		children: make([]navElement, 0),
-	}
+	e.name = name + "/"
+	e.title = name // będzie zastąpione jeżeli index.html jest w katalogu
+	e.children = make([]navElement, 0)
 
 	for _, c := range dirEntries {
 		if c.Type().IsDir() {
-			thisDir.children, err = directoryNavTree(dir+"/"+c.Name(), thisDir.children)
+			ce := navElement{}
+			err = directoryNavTree(dir+"/"+c.Name(), &ce)
 			if err != nil {
-				return nil, err
+				return err
 			}
-		} else if c.Name() != "index.html" {
+			e.children = append(e.children, ce)
+		} else {
 			title := getTitle(dir + "/" + c.Name())
 			if title == "" {
 				title = c.Name()
 			}
-			thisDir.children = append(thisDir.children, navElement{
-				name:     c.Name(),
-				link:     c.Name(),
-				title:    title,
-				children: nil,
-			})
+			if c.Name() == "index.html" {
+				e.title = title
+				e.link = true
+			} else {
+				e.children = append(e.children, navElement{
+					name:     c.Name(),
+					link:     true,
+					title:    title,
+					children: nil,
+				})
+			}
 		}
 	}
 
-	return append(parentsChildren, thisDir), nil
+	return nil
 }
 
 func main() {
-	n, err := directoryNavTree("src", make([]navElement, 0))
+	nav = navElement{}
+	err := directoryNavTree("src", &nav)
 	if err != nil {
 		log.Panicln("Error when creating navigation tree:", err)
 	}
-	nav = n[0]
 	fmt.Println(nav)
+	fmt.Println(nav.children)
 
 	err = filepath.WalkDir("src", handleFile)
 	if err != nil {
